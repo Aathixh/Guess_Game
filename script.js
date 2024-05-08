1b98b971234f690bc0672a04b72a18719291d214
Aathish R Viswam <aathishrv@gmail.com>
2024-05-08 18:40:02 +0530

    refactor: Update secretNumber variable declaration to let instead of const

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 'use strict';
 
-const secertNumber = Math.trunc(Math.random() * 20) + 1;
+let secertNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secertNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
+    //No Number
     document.querySelector('.message').textContent = '😒 No Number';
   } else if (guess === secertNumber) {
+    //Correct Number
     document.querySelector('.message').textContent = '👌Correct Guess!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secertNumber;
   } else if (guess > secertNumber) {
+    //Too High
     if (score > 1) {
       document.querySelector('.message').textContent = '✈️Too High!';
       score--;
@@ -21,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secertNumber) {
+    //Too Low
     if (score > 1) {
       document.querySelector('.message').textContent = '🤏Too Low!';
       score--;
@@ -30,4 +36,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secertNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
 });
